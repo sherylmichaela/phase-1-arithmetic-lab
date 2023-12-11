2f2f5989bc0721ce774b3c6809c75717210f28f8
Sheryl Chee <sherylmichaela@gmail.com>
2023-12-11 23:47:52 +1100

    Completed lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+console.log(Math.floor(mod));
+
+let max = Math.max(13, 17, 8, 20, 8);
+console.log(max);
